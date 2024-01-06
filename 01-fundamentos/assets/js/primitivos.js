11a242a095821fe1887514fbfa135238b232e6f5
Emilio del Hierro <delhierroemilio@gmail.com>
2024-01-06 16:42:07 -0500

    01-fundamentos/assets/js/primitivos.js agregado y carpetas organizadas

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,31 @@
+//string
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = "Ben Parker";
+console.log(nombre);
+
+nombre = `Tia May`;
+console.log(typeof nombre);
+
+//boolean
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+//number
+let edad = 33;
+console.log(typeof edad);
+
+//undefined
+let superPoder;
+console.log(typeof superPoder);
+
+//null
+let soyNull = null;
+console.log(typeof soyNull);
+
+//symbol
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+console.log(symbol1 === symbol2);
